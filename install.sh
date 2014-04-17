8f58b8e04c7b137a5ac051a5ea1fa6635e6accab
Santiago Blanco <santiblanko@users.noreply.github.com>
2014-04-16 20:16:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,145 +1,69 @@
-#!/usr/bin/env bash
-
-
-
 sudo apt-get update
+sudo apt-get upgrade
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+# Config mysql
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password toor'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password toor'
 
-
-sudo apt-get install -y vim curl python-software-properties
-
-
+# Install dependencies
+sudo apt-get install -y vim 
+sudo apt-get install -y curl 
+sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:ondrej/php5
-sudo apt-get install python-software-properties
-
-
 sudo apt-get update
+sudo apt-get install -y php5 
+sudo apt-get install -y apache2 
+sudo apt-get install -y libapache2-mod-php5 
+sudo apt-get install -y php5-curl 
+sudo apt-get install -y php5-gd 
+sudo apt-get install -y php5-mcrypt 
+sudo apt-get install -y php5-readline 
+sudo apt-get install -y mysql-server-5.5 
+sudo apt-get install -y php5-mysql 
+sudo apt-get install -y git-core 
+sudo apt-get install -y php5-xdebug
+sudo apt-get install -y redis-server
+sudo apt-get install -y memcached 
+sudo apt-get install -y beanstalkd
+sudo apt-get install -y supervisor
 
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl
-sudo apt-get install -y php5-gd php5-mcrypt php-pear mysql-server-5.5 php5-mysql git-core redis-server
-sudo apt-get install -y beanstalkd php5-xdebug
 
+# config beanstalkd
+sudo sed -i "s/#START=yes/START=yes/" /etc/default/beanstalkd
+sudo /etc/init.d/beanstalkd start
 
+# config xdebug
 cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
 xdebug.scream=1
 xdebug.cli_color=1
 xdebug.show_local_vars=1
 EOF
 
+# config php
 sudo a2enmod rewrite
-
-cd /home/vagrant
-mkdir Scripts && cd /home/vagrant/Scripts && mkdir PhpInfo
-sudo wget https://gist.githubusercontent.com/santiblanko/9815306/raw/78e267d2321a361b94d767cf4b15b8a8fbdba308/serve
-sudo mv serve taylorScript.sh
-sudo chmod +x taylorScript.sh
-
-cd /home/vagrant/ && mkdir virtuals
-sudo rm -rf /var/www
-sudo ln -fs /vagrant/public /var/www
-sudo ln -fs /vagrant/virtuals /home/vagrant/virtuals
-
-echo  "ServerName localhost"  > /etc/apache2/conf-available/servername.conf
-sudo a2enconf servername
-sudo service apache2 reload
-
-echo "--- Habilitando Errores de PHP. ---"
-
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
-sed -i 's/html//' /etc/apache2/sites-available/000-default.conf
-
-echo "--- Reiniciando Apache ---"
+sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
 
 sudo service apache2 restart
 
-echo "--- Instalando PHPUnit ---"
-
-wget https://phar.phpunit.de/phpunit.phar
-chmod +x phpunit.phar
-mv phpunit.phar /usr/local/bin/phpunit
-
-
-echo "--- Instalando composer ---"
+# composer install
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
-echo "--- Configurando beanstalkd  ---"
+# Added script and alias.
+cd /home/vagrant
+sudo wget https://gist.githubusercontent.com/santiblanko/9815306/raw/78e267d2321a361b94d767cf4b15b8a8fbdba308/serve
+sudo mv serve add-site.sh
+sudo chmod +x add-site.sh
 
-sudo sed -i "s/#START=yes/START=yes/" /etc/default/beanstalkd
-sudo /etc/init.d/beanstalkd start
+cat > /home/vagrant/.bash_aliases << EOF
+alias ..="cd .."
+alias ...="cd ../.."
+alias h='cd ~'
+alias c='clear'
 
-echo "--- Construyendo sitio con información de PHP ---"
-
-echo "<?php phpinfo();" > /home/vagrant/Scripts/PhpInfo/index.php
-
-sudo a2enmod rewrite
-
-echo "127.0.0.1  info.app" | sudo tee -a /etc/hosts
-
-vhost="<VirtualHost *:80>
-     ServerName info.app
-     DocumentRoot /home/vagrant/Scripts/PhpInfo
-     <Directory \"/home/vagrant/Scripts/PhpInfo\">
-          Order allow,deny
-          Allow from all
-          Require all granted
-          AllowOverride All
-    </Directory>
-</VirtualHost>"
-
-echo "$vhost" | sudo tee /etc/apache2/sites-available/info.app.conf
-
-sudo a2ensite info.app
-
-sudo /etc/init.d/apache2 restart
-
-echo "---- Instalando Beanstalkd Console ---"
-
-cd /home/vagrant/Scripts
-
-git clone https://github.com/ptrofimov/beanstalk_console.git Beansole
-
-echo "---- Asignando permisos ---"
-
-sudo chmod 777 /home/vagrant/Scripts/Beansole/storage.json
-
-sudo a2enmod rewrite
-
-echo "127.0.0.1  beansole.app" | sudo tee -a /etc/hosts
-
-vhost="<VirtualHost *:80>
-     ServerName beansole.app
-     DocumentRoot /home/vagrant/Scripts/Beansole/public
-     <Directory \"/home/vagrant/Scripts/Beansole/public\">
-          Order allow,deny
-          Allow from all
-          Require all granted
-          AllowOverride All
-    </Directory>
-</VirtualHost>"
-
-echo "$vhost" | sudo tee /etc/apache2/sites-available/beansole.app.conf
-
-sudo a2ensite beansole.app
-sudo /etc/init.d/apache2 restart
-
-sudo apt-get update
-sudo apt-get install -y  python g++ make
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y  nodejs
-
-echo "---- Instalando Grunt ---"
-sudo npm install -g grunt-cli
-
-echo "---- Instalando Yo ---"
-
-sudo npm install -g yo
-
-echo "---- Instalando Gulp ---"
-
-sudo npm install -g gulp
+function serve() {
+     sudo bash /vagrant/add-site.sh \$1 \$2
+}
+EOF
